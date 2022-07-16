49056e0529d4d97f1359e291ab6174790fb5134a
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2022-07-16 17:00:52 +0900

    Initial Commit

---
+++ AppLaunchTests/AppLaunchTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppLaunchTests.swift
+//  AppLaunchTests
+//
+//  Created by 山本響 on 2022/07/16.
+//
+
+import XCTest
+@testable import AppLaunch
+
+class AppLaunchTests: XCTestCase {
+
+    func test_emptyJustSoHaveAPassingTest() {
+        
+    }
+    
+}
---
+++ AppLaunchTests/TestingAppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  TestingAppDelegate.swift
+//  AppLaunch
+//
+//  Created by 山本響 on 2022/07/16.
+//
+
+import UIKit
+
+@objc(TestingAppDelegate)
+class TestingAppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        print(">> Launching with testing app delegate")
+        return true
+    }
+
+}
+
